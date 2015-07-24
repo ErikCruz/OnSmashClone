f775fbeac02de0940ddc29002071cd44ec436997
Erik Cruz <erikc1989@live.com>
2015-07-24 03:11:10 +0000

    Create login/register routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Videos' });
+});
+
+router.get('/login', function(req, res, next){
+   res.render('login', {title: 'Log In'}) 
+});
+
+router.get('/register', function(req, res, next){
+   res.render('register', {title: 'Register'}); 
 });
 
 module.exports = router;
