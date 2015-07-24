8efa8f8e5d45afbb0775ac850ff81a2110d18bc4
Erik Cruz <erikc1989@live.com>
2015-07-24 03:50:42 +0000

    Create videos route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Videos' });
+  //res.render('index', { title: 'Videos' });
+  res.redirect('/videos');
 });
 
 module.exports = router;
