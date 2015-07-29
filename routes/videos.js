346b01df73d2678fa7a7d4c0aac3f0b3082e416f
Erik Cruz <erikc1989@live.com>
2015-07-29 02:27:42 +0000

    Add new video form and route

--- routes/videos.js
+++ routes/videos.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var Videos = require('../models/video');
 var router = express.Router();
 
 // get all videos for index view
 router.get('/', function(req, res, next){
+    // Videos.find({}, function(err, vids){
+    //     if(err) throw err;
+    //     res.json(vids);
+    // });
     res.render('videos/index', {title: 'Videos'});    
+});
+
+// new video form
+router.get('/new', function(req, res, next){
+   res.render('videos/new',{title: 'New Video'}); 
 });
 
 // get single video
