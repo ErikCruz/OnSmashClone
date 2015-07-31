9da031a12340b39c2d9d0b8666cdd62942c50b34
Erik Cruz <erikc1989@live.com>
2015-07-31 04:47:50 +0000

    remove mongoose from main app file

--- app.js
+++ app.js
@@ -4,13 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://'+process.env.IP+'/onsmash');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-  console.log('successfully connected to database');
-});
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
